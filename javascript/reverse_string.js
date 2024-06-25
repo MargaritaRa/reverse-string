3b5a6b4da1ec7ab500a2178d1e46f648165a1186
Margarita Ramirez <margarita26.mr@gmail.com>
2024-06-24 20:18:29 -0400

    reverse

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,13 @@
 function reverseString(str) {
-  // type your code here
+  let reversed = '';
+
+  // loop through the string in reverse order
+  for(let i = str.length - 1; i >= 0; i--) {
+    // append each char to the reversed string
+    reversed += str[i]
+  }
+
+  return reversed;
 }
 
 if (require.main === module) {
@@ -17,3 +25,9 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Define a function reverseString that takes a single argument str.
+// Initialize an empty string reversed to store the reversed characters.
+// Loop through the characters of str starting from the last character to the first:
+// Append each character to reversed.
+// Return the reversed string.
